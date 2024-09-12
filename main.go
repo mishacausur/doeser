72ee2cb3d93768f8a7bad85f3bac4d3ec464dc0e
Misha Causur <mishacausur@mail.ru>
2024-09-12 22:48:36 +0500

    response w/ html

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func main() {
-	error := http.ListenAndServe(":7540", nil)
+
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = "7540"
+	}
+
+	indexPage := http.FileServer(http.Dir("./web"))
+
+	http.Handle("/", indexPage)
+
+	error := http.ListenAndServe(":"+port, nil)
 
 	if error != nil {
 		panic(error)
