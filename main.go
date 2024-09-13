f118aa00d40078903b010ee2b45deb66cafd66e2
Misha Causur <mishacausur@mail.ru>
2024-09-13 21:16:48 +0500

    added next date method

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -36,6 +39,52 @@ func createDatabase(db *sql.DB) error {
 	}
 
 	return nil
+}
+
+func NextDate(now time.Time, date string, repeat string) (string, error) {
+	layout := "20060102"
+	taskDate, err := time.Parse(layout, date)
+	if err != nil {
+		return "", fmt.Errorf("Ошибка парсинга даты: %v", err)
+	}
+	repeat = strings.TrimSpace(repeat)
+	if repeat == "" {
+		return "", nil
+	}
+	var nextDate time.Time
+
+	if repeat == "y" {
+		nextDate = taskDate
+		for !nextDate.After(now) {
+			nextDate = nextDate.AddDate(1, 0, 0)
+			if nextDate.Year() > now.Year()+100 {
+				return "", fmt.Errorf("Некорректная дата -- превышение лимита лет")
+			}
+		}
+	} else if strings.HasPrefix(repeat, "d ") {
+		fields := strings.Fields(repeat)
+		if len(fields) != 2 {
+			return "", fmt.Errorf("Некорректный формат повторения")
+		}
+		numberStrings := fields[1]
+		days, err := strconv.Atoi(numberStrings)
+		if err != nil {
+			return "", fmt.Errorf("Некорректный формат повторения")
+		}
+		if days <= 0 || days > 400 {
+			return "", fmt.Errorf("Некорректный формат повторения")
+		}
+		nextDate = taskDate
+		for !nextDate.After(now) {
+			nextDate = nextDate.AddDate(0, 0, days)
+			if nextDate.After(taskDate.AddDate(5, 0, 0)) {
+				return "", fmt.Errorf("Не удалось получить следующую дату")
+			}
+		}
+	} else {
+		return "", nil
+	}
+	return nextDate.Format(layout), nil
 }
 
 func main() {
